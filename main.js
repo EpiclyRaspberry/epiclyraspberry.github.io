12ddfc55d12307babdd584ca74ada9a352ac673b
EpiclyRaspberry <84133368+EpiclyRaspberry@users.noreply.github.com>
2022-05-14 00:49:58 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ function about(){
 }
 setInterval(widthLoop,2);
 function widthLoop(){
+try{
 var dropbtn = document.getElementById("hi");
 dropbtn.style.width = window.innerWidth-31.5;
-
+var top = document.getElementById("top");
+top.style.width = window.innerWidth-31.5;
+}catch{
+}
 }
