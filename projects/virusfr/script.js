e8b51e41bb1b26a895d70794912d3b01df259ff3
EpiclyRaspberry <raspberryepicly@gmail.com>
2023-09-06 20:07:52 +0800

    shopee

--- projects/virusfr/script.js
+++ projects/virusfr/script.js
@@ -3,7 +3,7 @@ let clicks = 0;
 document.addEventListener("click", function(e) {
     //opens https://virusfr.com/ in a new tab until 3 clicks
     if (clicks < 3) {
-        window.open("https://virusfr.com/", "_blank");
+        window.open("https://shopee.ph/", "_blank");
         clicks++;
     }
 });
